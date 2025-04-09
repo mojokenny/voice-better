524b828ee6686ca88977a48c5ee2d859a7a5a109
mojokenny <leoakenny@gmail.com>
2025-04-09 15:23:26 -0700

    Create layout.tsx

---
+++ app/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Better Voice - Customer Feedback Platform',
+  description: 'Collect real-time customer feedback in an efficient, discreet manner.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
